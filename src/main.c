89aff1da96a97f780105565a57f55308fdd48bfc
Borys Jelenski <borys.jelenski@gmail.com>
2020-04-02 15:27:03 +0200

    Blinky example

--- src/main.c
+++ src/main.c
@@ -1,7 +1,29 @@
-#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
 
-int main()
+#ifdef __cplusplus
+extern "C" {
+#endif
+	
+#include "nrf_delay.h"
+#include "nrf_gpio.h"
+	
+#ifdef __cplusplus
+}
+#endif
+
+
+int main(void)
 {
-  printf("Hello, CMake!");
-  return 0;    
+  const uint32_t led1_pin = NRF_GPIO_PIN_MAP(0, 13);
+  
+  nrf_gpio_cfg_output(led1_pin);
+
+	for (;;)
+	{
+  	nrf_gpio_pin_set(led1_pin);
+		nrf_delay_ms(500);
+  	nrf_gpio_pin_clear(led1_pin);
+		nrf_delay_ms(500);
+	}
 }
