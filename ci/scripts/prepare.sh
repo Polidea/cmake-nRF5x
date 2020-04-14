5c05343ec465492688c11121f649649922b7f7f0
borys.jelenski <borys.jelenski@gmail.com>
2020-04-14 18:50:47 +0200

    Check nRF5 tools available from PATH on Windows (Cygwin)

--- ci/scripts/prepare.sh
+++ ci/scripts/prepare.sh
@@ -21,9 +21,23 @@ else
     download_gcc_toolchain "$TOOLCHAINS_DIR/gcc"
 fi
 
-# Download nRF tools
-if [[ -d "$TOOLS_DIR/nrf" ]]; then
-    echo "nRF Tools already present, skipping..."
+# Check and optionally download nRF tools
+if [[ "$OSTYPE" == "cygwin" ]]; then
+    # nRF5 Command Line Tools for Windows are available only as an .exe installer; expect the user to install them manually
+    echo "Running on Windows (Cygwin), checking the nRF Command Line Tools available from PATH:"
+
+    (check_binary nrfjprog); nrfjprog_found="$?"
+    (check_binary mergehex); mergehex_found="$?"
+
+    if [[ "$nrfjprog_found" -ne 0 ]] || [[ "$mergehex_found" -ne 0 ]]; then
+        echo "Some of the nRF5 Command Line Tools are missing. Please install the nRF Command Line Tools manually and then re-run this script."
+        exit 1
+    fi
 else
-    download_nrf_tools "$TOOLS_DIR/nrf"
+    # Download nRF tools
+    if [[ -d "$TOOLS_DIR/nrf" ]]; then
+        echo "nRF Tools already present, skipping..."
+    else
+        download_nrf_tools "$TOOLS_DIR/nrf"
+    fi
 fi
