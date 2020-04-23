3597f0034ad6e64fe1c3cf2fb35c416d8a4c6a64
borys.jelenski <borys.jelenski@gmail.com>
2020-04-23 05:06:02 +0200

    build_all.sh builds examples for every SoftDevice configuration (SD variant) available for a particular board; renamed variables

--- ci/scripts/build_all.sh
+++ ci/scripts/build_all.sh
@@ -3,12 +3,12 @@
 source "${BASH_SOURCE%/*}/common/build.sh"
 
 # Collect relative paths to the examples.
-examples_dirs=()
+example_local_dirs=()
 pushd "$EXAMPLES_DIR" > /dev/null
     for example in `find . -name "CMakeLists.txt"`; do
         example_dir=`dirname $example`
         # Strip the '.' prefix (current directory) from the example directory
-        examples_dirs+=(${example_dir#./})
+        example_local_dirs+=(${example_dir#./})
     done
 popd > /dev/null
 
@@ -16,13 +16,26 @@ popd > /dev/null
 for sdk in `ls -d $SDKS_DIR/*/`; do
     sdk_version=`basename $sdk`
     # For each SDK, try to apply examples
-    for example in "${examples_dirs[@]}"; do
+    for example in "${example_local_dirs[@]}"; do
         sdk_example_dir="$SDKS_DIR/$sdk_version/examples/$example"
 
-        # For each varaint
-        for variant_dir in `ls -d $sdk_example_dir/pca*`; do
-            variant=`basename $variant_dir`
-                build_example "$example" "$sdk_version" "$variant"
+        # For each board
+        for board_dir in `ls -d $sdk_example_dir/pca*`; do
+            board=`basename $board_dir`
+
+            # Identify SoftDevice variants available for the specified board (SoftDevice configurations only).
+            supported_sd_variant_dirs=($(ls -d $board_dir/s* 2> /dev/null))
+
+            if [[ ${#supported_sd_variant_dirs[@]} -eq 0 ]]; then
+                echo "A SoftDevice configuration for the '$example' for the '$board' board is not available. Non-SoftDevice builds are currently not supported. Build skipped."
+                continue
+            fi
+
+            # For each SDK variant
+            for sd_variant_dir in ${supported_sd_variant_dirs[@]}; do
+                sd_variant=`basename $sd_variant_dir`
+                build_example "$example" "$sdk_version" "$board" "$sd_variant"
+            done
         done
     done
 done
