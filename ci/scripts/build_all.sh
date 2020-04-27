c3a83bfd44c5103d74e78ed0b4aeb6c4f34db94a
borys.jelenski <borys.jelenski@gmail.com>
2020-04-27 05:11:27 +0200

    When building all examples, use both local (custom) and SDK configurations

--- ci/scripts/build_all.sh
+++ ci/scripts/build_all.sh
@@ -2,6 +2,39 @@
 
 source "${BASH_SOURCE%/*}/common/build.sh"
 
+function build_all_configs() {
+    local example=$1
+    local sdk_version=$2
+    local toolchain=$3
+    local config_dir=$4
+
+    if [[ ! -d $config_dir ]]; then
+        echo "\"$config_dir\" is not a valid configuration directory"
+        return 1
+    fi
+
+    # For each board
+    for board_dir in `ls -d $config_dir/pca*`; do
+        board=`basename $board_dir`
+
+        # Identify SoftDevice variants available for the specified board (SoftDevice configurations only)
+        supported_sd_variant_dirs=($(ls -d $board_dir/s* 2> /dev/null))
+
+        if [[ ${#supported_sd_variant_dirs[@]} -eq 0 ]]; then
+            echo "A SoftDevice configuration for the '$example' for the '$board' board is not available (config. dir.: $config_dir). Build skipped."
+            continue
+        fi
+
+        # For each SDK variant
+        for sd_variant_dir in ${supported_sd_variant_dirs[@]}; do
+            sd_variant=`basename $sd_variant_dir`
+            build_example $example $sdk_version $board $sd_variant $toolchain $config_dir || exit 1
+        done
+    done
+
+    return 0
+}
+
 sdk_versions=()
 
 if [[ $# -gt 0 ]]; then
@@ -41,26 +74,13 @@ popd > /dev/null
 # Applying examples to included SDKs
 for sdk_ver in "${sdk_versions[@]}"; do
     # For each SDK, try to apply examples
-    for example in "${example_local_dirs[@]}"; do 
-        sdk_example_dir="$SDKS_DIR/$sdk_ver/examples/$example"
+    for example in "${example_local_dirs[@]}"; do
+        # Build all custom configs in the local example directory
+        if [[ -d "$EXAMPLES_DIR/$example/config" ]]; then
+            build_all_configs $example $sdk_ver "gcc" "$EXAMPLES_DIR/$example/config"
+        fi
 
-        # For each board
-        for board_dir in `ls -d $sdk_example_dir/pca*`; do
-            board=`basename $board_dir`
-
-            # Identify SoftDevice variants available for the specified board (SoftDevice configurations only).
-            supported_sd_variant_dirs=($(ls -d $board_dir/s* 2> /dev/null))
-
-            if [[ ${#supported_sd_variant_dirs[@]} -eq 0 ]]; then
-                echo "A SoftDevice configuration for the '$example' for the '$board' board is not available. Non-SoftDevice builds are currently not supported. Build skipped."
-                continue
-            fi
-
-            # For each SDK variant
-            for sd_variant_dir in ${supported_sd_variant_dirs[@]}; do
-                sd_variant=`basename $sd_variant_dir`
-                build_example "$example" "$sdk_ver" "$board" "$sd_variant" || exit 1
-            done
-        done
+        # Build all configs in the SDK example directory
+        build_all_configs $example $sdk_ver "gcc" "$SDKS_DIR/$sdk_ver/examples/$example"
     done
 done
