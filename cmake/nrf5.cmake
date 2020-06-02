7a2c7eb83b30e73d8dd5fb78d8e74700fea0c48e
Borys Jeleński <borys.jelenski@polidea.com>
2020-06-02 21:14:06 +0200

    Added support for NRF5_STACK_SIZE cache variable

--- cmake/nrf5.cmake
+++ cmake/nrf5.cmake
@@ -130,6 +130,16 @@ if(NRF5_APPCONFIG_PATH)
   message(STATUS "Using app_config.h include path: ${NRF5_APPCONFIG_PATH}")
 endif()
 
+# Stack size
+set(NRF5_STACK_SIZE "" CACHE STRING "Max. size of the stack (in bytes), defines `__STACK_SIZE` accordingly. If not specified, a default value for the specified target will be used.")
+if(NRF5_STACK_SIZE)
+  if(NOT NRF5_STACK_SIZE MATCHES "^[1-9][0-9]*$")
+    message(FATAL_ERROR "NRF5_STACK_SIZE must be a positive decimal integer")
+  endif()
+  add_compile_options("-D__STACK_SIZE=${NRF5_STACK_SIZE}")
+  message(STATUS "Using stack size: ${NRF5_STACK_SIZE}")
+endif()
+
 # NRFJPROG executable
 set(NRF5_NRFJPROG "" CACHE FILEPATH "nrfjprog utility executable file. If not specified, it is assumed that nrfjprog is available from PATH.")
 if(NOT NRF5_NRFJPROG)
