329b413bd96eca3168e528d77acf7cb056781fc5
Przemysław Lenart <przemek.lenart@polidea.com>
2020-06-16 14:31:47 +0200

    Add support for groups creation.
    * New field in JSON format was added: "groups" which specifies a list of groups associated with specific library.
    * New nrf5_groups.cmake file is automatically generated which provides nrf5_add_group_XXX functions.

--- ci/scripts/generate_cmake.sh
+++ ci/scripts/generate_cmake.sh
@@ -6,11 +6,9 @@ source "${BASH_SOURCE%/*}/common/consts.sh"
 source "${BASH_SOURCE%/*}/common/check_deps.sh"
 source "${BASH_SOURCE%/*}/common/python.sh"
 
-for file_path in $(find "${LIBRARIES_DIR}" -name "*.json"); do
-    file_name="$(basename ${file_path})"
-    python3 "${PYTHON_DIR}/generate_cmake.py" \
-        --supported_sdks "15.3.0" "16.0.0" \
-        --template "${LIBRARIES_CMAKE_TEMPLATE_FILE}" \
-        --input "${file_path}" \
-        --output "${CMAKE_DIR}/${file_name%.*}.cmake"
-done
+python3 "${PYTHON_DIR}/generate_cmake.py" \
+    --supported_sdks "15.3.0" "16.0.0" \
+    --lib_template "${LIBRARIES_CMAKE_TEMPLATE_FILE}" \
+    --group_template "${LIBRARIES_CMAKE_GROUPS_TEMPLATE_FILE}" \
+    --input_dir "${LIBRARIES_DIR}" \
+    --output_dir "${CMAKE_DIR}"
