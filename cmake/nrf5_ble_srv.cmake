8f75e8977a1e773f7a87e2477055e265ba7f67e6
Przemysław Lenart <przemek.lenart@polidea.com>
2020-06-25 14:40:27 +0200

    Add ble_app_bps example and nrf5_ble_srv_bps library.

--- cmake/nrf5_ble_srv.cmake
+++ cmake/nrf5_ble_srv.cmake
@@ -660,3 +660,51 @@ if(NRF5_SDK_VERSION VERSION_EQUAL 16.0.0)
     nrf5_queue
   )
 endif()
+
+# BLE Blood Pressure Service (Peripheral)
+add_library(nrf5_ble_srv_bps OBJECT EXCLUDE_FROM_ALL
+  "${NRF5_SDK_PATH}/components/ble/ble_services/ble_bps/ble_bps.c"
+)
+target_include_directories(nrf5_ble_srv_bps PUBLIC
+  "${NRF5_SDK_PATH}/components/ble/ble_services/ble_bps"
+)
+target_link_libraries(nrf5_ble_srv_bps PUBLIC
+  nrf5_ble_common
+)
+if(NRF5_SDK_VERSION VERSION_EQUAL 16.0.0)
+  
+  target_link_libraries(nrf5_ble_srv_bps PUBLIC
+    nrf5_ble_gq
+  )
+endif()
+list(APPEND NRF5_LIBRARY_NRF5_BLE_SRV_BPS_DEPENDENCIES
+  nrf5_app_scheduler
+  nrf5_app_timer
+  nrf5_app_util_platform
+  nrf5_atflags
+  nrf5_atomic
+  nrf5_ble_common
+  nrf5_ble_srv_bps
+  nrf5_config
+  nrf5_delay
+  nrf5_log_fwd
+  nrf5_mdk
+  nrf5_nrfx_common
+  nrf5_nrfx_hal
+  nrf5_sdh
+  nrf5_section
+  nrf5_soc
+  nrf5_strerror
+)
+if(NRF5_SDK_VERSION VERSION_EQUAL 16.0.0)
+  list(APPEND NRF5_LIBRARY_NRF5_BLE_SRV_BPS_DEPENDENCIES
+    nrf5_balloc
+    nrf5_balloc_fwd
+    nrf5_ble_gq
+    nrf5_cli_fwd
+    nrf5_ext_fprintf
+    nrf5_memobj
+    nrf5_memobj_fwd
+    nrf5_queue
+  )
+endif()
