53bf11f7b28a25e3a1e077238ba404ecc496dd40
Borys Jeleński <borys.jelenski@polidea.com>
2020-06-26 04:04:47 +0200

    Add lib tests: nrf_block_dev_qspi, nrf_block_dev_ram, nrf_block_dev_sdc, nrf_serial

---
+++ ci/libraries_tests/nrf5_block_dev_qspi/main.c
@@ -0,0 +1,9 @@
+#include "nrf_block_dev.h"
+#include "nrf_block_dev_qspi.h"
+
+int main()
+{
+    nrf_block_dev_qspi_ops_get(NULL);
+
+    return 0;
+}
---
+++ ci/libraries_tests/nrf5_block_dev_sdc/main.c
@@ -0,0 +1,9 @@
+#include "nrf_block_dev.h"
+#include "nrf_block_dev_sdc.h"
+
+int main()
+{
+    nrf_block_dev_sdc_ops_get(NULL);
+
+    return 0;
+}
