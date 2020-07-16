d81b0f7d81b25daff200c3480a2ef48794fe8803
Borys Jeleński <borys.jelenski@polidea.com>
2020-07-16 04:47:52 +0200

    Added 'nrf_dfu_handling_error' library

---
+++ ci/libraries_tests/nrf5_dfu_handling_error/main.c
@@ -0,0 +1,7 @@
+#include "nrf_dfu_handling_error.h"
+
+int main()
+{
+  ext_error_get();
+  return 0;
+}
--- cmake/nrf5_dfu.cmake
+++ cmake/nrf5_dfu.cmake
@@ -396,3 +396,46 @@ list(APPEND NRF5_LIBRARY_NRF5_DFU_VALIDATION_DEPENDENCIES
   nrf5_stack_info
   nrf5_strerror
 )
+
+# DFU result codes
+add_library(nrf5_dfu_handling_error OBJECT EXCLUDE_FROM_ALL
+  "${NRF5_SDK_PATH}/components/libraries/bootloader/dfu/nrf_dfu_handling_error.c"
+)
+target_include_directories(nrf5_dfu_handling_error PUBLIC
+  "${NRF5_SDK_PATH}/components/libraries/bootloader/dfu"
+)
+target_link_libraries(nrf5_dfu_handling_error PUBLIC
+  nrf5_log
+)
+list(APPEND NRF5_LIBRARY_NRF5_DFU_HANDLING_ERROR_DEPENDENCIES
+  nrf5_app_scheduler
+  nrf5_app_util_platform
+  nrf5_atfifo
+  nrf5_atomic
+  nrf5_balloc
+  nrf5_balloc_fwd
+  nrf5_cli
+  nrf5_cli_fwd
+  nrf5_config
+  nrf5_crc16
+  nrf5_delay
+  nrf5_dfu_handling_error
+  nrf5_ext_fprintf
+  nrf5_fds
+  nrf5_fstorage
+  nrf5_log
+  nrf5_log_fwd
+  nrf5_mdk
+  nrf5_memobj
+  nrf5_memobj_fwd
+  nrf5_mtx
+  nrf5_nrfx_common
+  nrf5_nrfx_hal
+  nrf5_pwr_mgmt
+  nrf5_queue
+  nrf5_ringbuf
+  nrf5_sdh
+  nrf5_section
+  nrf5_soc
+  nrf5_strerror
+)
