df73f85374c96162e65222bbd1ca75672702affc
Borys Jeleński <borys.jelenski@polidea.com>
2020-07-17 02:34:57 +0200

    Added 'nrf_dfu_transport' lib

--- cmake/nrf5_dfu.cmake
+++ cmake/nrf5_dfu.cmake
@@ -516,3 +516,46 @@ list(APPEND NRF5_LIBRARY_NRF5_DFU_REQ_HANDLER_DEPENDENCIES
   nrf5_stack_info
   nrf5_strerror
 )
+
+# DFU transport
+add_library(nrf5_dfu_transport OBJECT EXCLUDE_FROM_ALL
+  "${NRF5_SDK_PATH}/components/libraries/bootloader/dfu/nrf_dfu_transport.c"
+)
+target_include_directories(nrf5_dfu_transport PUBLIC
+  "${NRF5_SDK_PATH}/components/libraries/bootloader/dfu"
+)
+target_link_libraries(nrf5_dfu_transport PUBLIC
+  nrf5_log
+)
+list(APPEND NRF5_LIBRARY_NRF5_DFU_TRANSPORT_DEPENDENCIES
+  nrf5_app_scheduler
+  nrf5_app_util_platform
+  nrf5_atfifo
+  nrf5_atomic
+  nrf5_balloc
+  nrf5_balloc_fwd
+  nrf5_cli
+  nrf5_cli_fwd
+  nrf5_config
+  nrf5_crc16
+  nrf5_delay
+  nrf5_dfu_transport
+  nrf5_ext_fprintf
+  nrf5_fds
+  nrf5_fstorage
+  nrf5_log
+  nrf5_log_fwd
+  nrf5_mdk
+  nrf5_memobj
+  nrf5_memobj_fwd
+  nrf5_mtx
+  nrf5_nrfx_common
+  nrf5_nrfx_hal
+  nrf5_pwr_mgmt
+  nrf5_queue
+  nrf5_ringbuf
+  nrf5_sdh
+  nrf5_section
+  nrf5_soc
+  nrf5_strerror
+)
