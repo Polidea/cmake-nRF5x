f559cba9f6da22d2258e43eab04cb0c8b1ee93d3
Borys Jeleński <borys.jelenski@polidea.com>
2020-07-20 06:55:52 +0200

    Added `nrf_bootloader` lib

--- cmake/nrf5_dfu.cmake
+++ cmake/nrf5_dfu.cmake
@@ -986,3 +986,82 @@ list(APPEND NRF5_LIBRARY_NRF5_BOOTLOADER_APP_START_DEPENDENCIES
   nrf5_soc
   nrf5_strerror
 )
+
+# Bootloader modules
+add_library(nrf5_bootloader OBJECT EXCLUDE_FROM_ALL
+  "${NRF5_SDK_PATH}/components/libraries/bootloader/nrf_bootloader.c"
+)
+target_include_directories(nrf5_bootloader PUBLIC
+  "${NRF5_SDK_PATH}/components/libraries/bootloader"
+)
+target_link_libraries(nrf5_bootloader PUBLIC
+  nrf5_app_scheduler
+  nrf5_boards
+  nrf5_bootloader_app_start
+  nrf5_bootloader_fw_activation
+  nrf5_dfu_validation
+  nrf5_log
+  nrf5_nrfx_hal
+)
+list(APPEND NRF5_LIBRARY_NRF5_BOOTLOADER_DEPENDENCIES
+  nrf5_app_scheduler
+  nrf5_app_util_platform
+  nrf5_assert
+  nrf5_atfifo
+  nrf5_atomic
+  nrf5_balloc
+  nrf5_balloc_fwd
+  nrf5_boards
+  nrf5_bootloader
+  nrf5_bootloader_app_start
+  nrf5_bootloader_dfu_timers
+  nrf5_bootloader_fw_activation
+  nrf5_bootloader_info
+  nrf5_bootloader_wdt
+  nrf5_cli
+  nrf5_cli_fwd
+  nrf5_config
+  nrf5_crc16
+  nrf5_crc32
+  nrf5_crypto
+  nrf5_delay
+  nrf5_dfu_cc_pb
+  nrf5_dfu_flash
+  nrf5_dfu_mbr
+  nrf5_dfu_settings
+  nrf5_dfu_utils
+  nrf5_dfu_validation
+  nrf5_dfu_ver_validation
+  nrf5_ext_cc310_bl_fwd
+  nrf5_ext_cc310_fwd
+  nrf5_ext_cifra_aes128_eax_fwd
+  nrf5_ext_fprintf
+  nrf5_ext_mbedtls_fwd
+  nrf5_ext_micro_ecc_fwd
+  nrf5_ext_nanopb
+  nrf5_ext_oberon_fwd
+  nrf5_ext_optiga_fwd
+  nrf5_fds
+  nrf5_fstorage
+  nrf5_fstorage_nvmc
+  nrf5_fstorage_sd
+  nrf5_log
+  nrf5_log_fwd
+  nrf5_mdk
+  nrf5_mem_manager
+  nrf5_memobj
+  nrf5_memobj_fwd
+  nrf5_mtx
+  nrf5_nrfx_common
+  nrf5_nrfx_hal
+  nrf5_nrfx_nvmc
+  nrf5_pwr_mgmt
+  nrf5_queue
+  nrf5_ringbuf
+  nrf5_sdh
+  nrf5_section
+  nrf5_sha256_fwd
+  nrf5_soc
+  nrf5_stack_info
+  nrf5_strerror
+)
